b2e2ca1d0118e35266856c7a1dd918f21442323f
TomiRenaldi <tomirenaldi2@gmail.com>
2024-11-18 15:37:46 +0700

    :lipstick: Add rencana apa yang kami pakai > Tile

--- components/works.tsx
+++ components/works.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { TileBackground, TileContent, TileWrapper } from "./tile";
+import { Tile, TileBackground, TileContent, TileWrapper } from "./tile";
 
 const Works = () => (
-    <TileWrapper numOfPages={3}>
-        <TileBackground></TileBackground>
-        <TileContent>Foooooo</TileContent>
-        <TileContent>Foooooo</TileContent>
-    </TileWrapper>
-)
+  <TileWrapper numOfPages={3}>
+    <TileBackground></TileBackground>
+    <TileContent>
+      <Tile
+        page={0}
+        renderContent={({ progress }) => (
+          <span className="text-9xl">Foo {progress}</span>
+        )}
+      ></Tile>
+      <Tile
+        page={1}
+        renderContent={({ progress }) => (
+          <span className="text-9xl">Barrr {progress}</span>
+        )}
+      ></Tile>
+      <Tile
+        page={2}
+        renderContent={({ progress }) => (
+          <span className="text-9xl">eadwawdaw {progress}</span>
+        )}
+      ></Tile>
+    </TileContent>
+  </TileWrapper>
+);
 
 export default Works;
